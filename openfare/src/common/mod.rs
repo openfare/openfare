54bdec811e84bcfc6d40b4a93a88f27551bac230
rndhouse <rndhouse@protonmail.com>
2022-02-19 18:47:04 +0000

    Remove usless documentations.

--- openfare/src/common/mod.rs
+++ openfare/src/common/mod.rs
@@ -1,10 +1,3 @@
-//! A module for data structures which are available to all super modules.
-//!
-//! This module contains data structures which are available to all super modules.
-//! The number of data structures in this module should be minimized. The data structures
-//! should be as simple as possible.
-//!
-//! Print statements are prohibited within this module. Logging is allowed.
 pub mod config;
 pub mod fs;
 pub mod git;
