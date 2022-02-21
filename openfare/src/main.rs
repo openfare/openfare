0919aaae6f4a789d8e16d11b84bb454508941a79
rndhouse <rndhouse@protonmail.com>
2022-02-21 01:53:58 +0000

    WIP

--- openfare/src/main.rs
+++ openfare/src/main.rs
@@ -7,6 +7,7 @@ mod config;
 mod extensions;
 mod handles;
 mod price;
+mod services;
 mod setup;
 
 fn main() {
--- openfare/src/services/mod.rs
+++ openfare/src/services/mod.rs
@@ -1,3 +1,4 @@
+mod config;
 mod lnpay;
 mod portal;
 
@@ -8,16 +9,27 @@ pub enum ServiceType {
     LnPay,
 }
 
-/// Payment services.
+pub fn pay(
+    package_locks: &std::collections::BTreeMap<
+        openfare_lib::package::Package,
+        openfare_lib::lock::Lock,
+    >,
+    // service_type: 
+    config: &crate::config::Config,
+) {
+    // config.services.default
+}
+
+/// Payment services config.
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-pub struct Services {
+pub struct Config {
     pub default: ServiceType,
     pub portal: portal::Portal,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub lnpay: Option<lnpay::LnPay>,
 }
 
-impl std::default::Default for Services {
+impl std::default::Default for Config {
     fn default() -> Self {
         Self {
             default: ServiceType::Portal,
@@ -27,7 +39,7 @@ impl std::default::Default for Services {
     }
 }
 
-impl std::fmt::Display for Services {
+impl std::fmt::Display for Config {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
