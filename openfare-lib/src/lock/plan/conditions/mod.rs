e0c3e9b70f7eae09def5d641ed4328b1b992b472
rndhouse <rndhouse@protonmail.com>
2022-03-29 00:28:49 +0100

    Move conditions parameters into separate module.

--- openfare-lib/src/lock/plan/conditions/mod.rs
+++ openfare-lib/src/lock/plan/conditions/mod.rs
@@ -4,11 +4,13 @@ mod common;
 mod employees_count;
 mod expiration;
 mod for_profit;
+mod parameters;
 
 pub use common::{Condition, ConditionMetadata};
 pub use employees_count::EmployeesCount;
 pub use expiration::Expiration;
 pub use for_profit::ForProfit;
+pub use parameters::Parameters;
 
 #[derive(Debug, Default, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Conditions {
@@ -64,41 +66,5 @@ impl Conditions {
         if self.employees_count.is_none() {
             self.employees_count = incoming.employees_count.clone();
         }
-    }
-}
-
-#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
-pub struct Parameters {
-    #[serde(rename = "employees-count")]
-    pub employees_count: Option<usize>,
-
-    #[serde(rename = "for-profit")]
-    pub for_profit: Option<bool>,
-
-    // TODO: remove.
-    pub commercial: bool,
-
-    #[serde(rename = "include-voluntary-donations")]
-    pub include_voluntary_plans: bool,
-}
-
-impl std::default::Default for Parameters {
-    fn default() -> Self {
-        Self {
-            employees_count: None,
-            for_profit: None,
-            commercial: true,
-            include_voluntary_plans: true,
-        }
-    }
-}
-
-impl std::fmt::Display for Parameters {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            serde_json::to_string_pretty(&self).map_err(|_| std::fmt::Error::default())?
-        )
     }
 }
---
+++ openfare-lib/src/lock/plan/conditions/parameters.rs
@@ -0,0 +1,35 @@
+#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct Parameters {
+    #[serde(rename = "employees-count")]
+    pub employees_count: Option<usize>,
+
+    #[serde(rename = "for-profit")]
+    pub for_profit: Option<bool>,
+
+    // TODO: remove.
+    pub commercial: bool,
+
+    #[serde(rename = "include-voluntary-donations")]
+    pub include_voluntary_plans: bool,
+}
+
+impl std::default::Default for Parameters {
+    fn default() -> Self {
+        Self {
+            employees_count: None,
+            for_profit: None,
+            commercial: true,
+            include_voluntary_plans: true,
+        }
+    }
+}
+
+impl std::fmt::Display for Parameters {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            serde_json::to_string_pretty(&self).map_err(|_| std::fmt::Error::default())?
+        )
+    }
+}
