ed2faa72fa049d0c45d5cd97a244c9a66ddb71ec
rndhouse <rndhouse@protonmail.com>
2022-04-11 21:27:59 +0100

    Fix duplicate use of short argument name.

--- openfare/src/command/lock/common.rs
+++ openfare/src/command/lock/common.rs
@@ -3,6 +3,6 @@ use structopt::{self, StructOpt};
 #[derive(Debug, StructOpt, Clone)]
 pub struct LockFilePathArg {
     /// Lock file path. Searches in current working directory if not given.
-    #[structopt(name = "lock-file-path", long, short = "p")]
+    #[structopt(name = "lock-file-path", long)]
     pub path: Option<std::path::PathBuf>,
 }
