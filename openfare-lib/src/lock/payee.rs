515171e5bf65dc7885e6dca12cf72b86af75d698
rndhouse <rndhouse@protonmail.com>
2022-04-11 17:25:46 +0100

    Refactor lock payee module from a directory into a file.

---
+++ openfare-lib/src/lock/payee.rs
@@ -0,0 +1,31 @@
+pub type Label = String;
+pub type Payees = std::collections::BTreeMap<Label, Payee>;
+pub type PaymentMethodName = String;
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct Payee {
+    pub url: Option<String>,
+    #[serde(flatten)]
+    pub profile: crate::profile::Profile,
+}
+
+pub fn get_lock_payee(
+    profile: &crate::profile::Profile,
+    all_lock_payees: &std::collections::BTreeMap<Label, Payee>,
+) -> Option<(Label, Payee)> {
+    for (name, existing_payee) in all_lock_payees {
+        if profile.unique_id == existing_payee.profile.unique_id {
+            return Some((name.clone(), existing_payee.clone()));
+        }
+    }
+    None
+}
+
+pub fn unique_label(payee_label: &Label, payee: &Payee) -> Label {
+    let unique_id = payee.profile.unique_id.to_string()[..13].to_string();
+    format!(
+        "{payee_label}___{unique_id}",
+        payee_label = payee_label,
+        unique_id = unique_id
+    )
+}
