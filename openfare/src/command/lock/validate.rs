98716e8a8fe2f4fcebf2be4215df62e0eb2e883a
rndhouse <rndhouse@protonmail.com>
2022-04-25 00:54:22 +0100

    Use lock handle to find and load lock file.

--- openfare/src/command/lock/validate.rs
+++ openfare/src/command/lock/validate.rs
@@ -1,7 +1,5 @@
 use super::common;
-use crate::handles::lock;
 use anyhow::Result;
-use serde_json::Value;
 use structopt::{self, StructOpt};
 
 #[derive(Debug, StructOpt, Clone)]
@@ -12,39 +10,14 @@ pub struct Arguments {
 }
 
 pub fn run_command(args: &Arguments) -> Result<()> {
-    let result = validate_lock_file(&args.lock_file_args);
-    if result.is_ok() {
-        println!("Lockfile is valid! ✅");
-    }
-    result
-}
-
-pub fn validate_lock_file(maybe_lock_file_path: &common::LockFilePathArg) -> Result<()> {
-    let lock_file_pathbuf = get_lock_file_pathbuf(maybe_lock_file_path)?;
-
-    let lock_file_string = file_to_string(lock_file_pathbuf.to_str().unwrap());
-    validate_lock_file_string(lock_file_string)
-}
-
-pub fn validate_lock_file_json(lock_file_json: Value) -> Result<()> {
-    let lock: openfare_lib::lock::Lock = serde_json::from_value(lock_file_json)?;
-    lock.validate()
-}
-
-pub fn validate_lock_file_string(lock_file_string: String) -> Result<()> {
-    validate_lock_file_json(serde_json::from_str(&lock_file_string).unwrap())
-}
-
-fn get_lock_file_pathbuf(
-    maybe_lock_file_path: &common::LockFilePathArg,
-) -> Result<std::path::PathBuf> {
-    let lockfile_pathbuf = match &maybe_lock_file_path.path {
-        None => lock::find_lock_file()?.unwrap(),
-        Some(a) => a.to_path_buf(),
+    let lock_handle = crate::handles::LockHandle::load(&args.lock_file_args.path)?;
+    match lock_handle.lock.validate() {
+        Ok(_) => {
+            println!("Lockfile is valid! ✅")
+        }
+        Err(e) => {
+            println!("Error validating lock file: {}", e)
+        }
     };
-    Ok(lockfile_pathbuf)
-}
-
-fn file_to_string(file_path: &str) -> String {
-    std::fs::read_to_string(file_path).unwrap()
+    Ok(())
 }
